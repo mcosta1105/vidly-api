8eb1b030cbd4c7574a6302b608d309cedccf68e3
Maycon <costa.maycon@live.com>
2019-06-04 22:30:51 +1000

    User implemented

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 //DB Connection
-mongoose.connect('mongodb://localhost/vidly')
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
     .then(()=> console.log('Connected to MongoBD...'))
     .catch(err => console.error(`Could not connect do DB..., ${err}`));
 
@@ -14,6 +14,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
 //Middlewares
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`) );
